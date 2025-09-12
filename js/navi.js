d04c7aa5a7b3abb7ef902966f536c3e13ae03ca6
sohee <sooooo4645@gmail.com>
2025-09-12 17:26:03 +0900

    team 마무리

--- js/navi.js
+++ js/navi.js
@@ -11,3 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
     mobileMenu.classList.remove("active");
   });
 });
+
+
+// AOS Js (스크롤 감지)
+  AOS.init({
+    duration: 1000, // 애니메이션 시간 (ms)
+    once: false,
+    easing: 'ease-out-cubic',
+      offset: 120       // 화면에서 몇 px 떨어졌을 때 시작할지
+  });
---
+++ js/team.js
@@ -0,0 +1,17 @@
+const teamSwiper = new Swiper(".teamSwiper", {
+
+  breakpoints :{
+    1024: {
+      slidesPerView: 1.8,
+    },
+    // // 태블릿
+    // 768: {
+    //   slidesPerView: 2,
+    // },
+    // 모바일
+    0: {
+      slidesPerView: 1.2,
+    },
+  },
+});
+
