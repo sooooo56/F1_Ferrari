3487025c6be8953bbd09508af5f161f717e2a5e4
sooooo56 <sooooo4645@gmail.com>
2025-09-16 23:04:04 +0900

    SF-25  머신 영역 진행 중

--- js/navi.js
+++ js/navi.js
@@ -18,5 +18,5 @@ document.addEventListener("DOMContentLoaded", () => {
     duration: 1000, // 애니메이션 시간 (ms)
     once: false,
     easing: 'ease-out-cubic',
-      offset: 120       // 화면에서 몇 px 떨어졌을 때 시작할지
+      // offset: 120       // 화면에서 몇 px 떨어졌을 때 시작할지
   });
